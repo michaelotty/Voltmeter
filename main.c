11f5c08c0d4976ec503a76c98951c2a4e367a422
mo360 <mo360@EEPC-267.campus.bath.ac.uk>
2016-04-18 15:07:01 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: mo360
+ *
+ * Created on 18 April 2016, 15:05
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
