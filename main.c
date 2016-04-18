7b7bc11ae89f01b08b69ecf869815b3eaaf8e511
mo360 <mo360@EEPC-267.campus.bath.ac.uk>
2016-04-18 17:04:50 +0100

    end of lab

--- main.c
+++ main.c
@@ -3,26 +3,44 @@
 #pragma config  PWRTE = ON
 #pragma config  CP = OFF
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <xc.h>
 #include "adc.h"
 #include "lcd.h"
 
 #define _XTAL_FREQ  4000000
+char* floatToString()
+{
+    char *str = newchar[10];
+    float floatingVoltage 
+}
 
 void main()
 {
-    TRISA = 0xFF;
+    TRISA = 0b00000100;
     TRISB = 0x00;    
     
+    CS = 1;
+    CLK = 0;
     RW = 0;    
     Lcd_Init();
     Lcd_Clear();
     
-    unsigned int voltage = readADC();
-    Lcd_Set_Cursor(1,1);
-    
-    char *string = "Voltage: 4.111";
-    Lcd_Write_String(string);
+    unsigned int voltage = 5;
+    float floatingVoltage = 0;
+    while(1)
+    {
+        Lcd_Clear();
+        
+        Lcd_Set_Cursor(1,1);
+        char *string = "Voltage:";
+        
+        Lcd_Write_String(string);
+        Lcd_Set_Cursor(2,1);
+        
+        floatingVoltage = readADC() * 4.5;
+        char *str = newchar[10];
+        sprintf(str, "%.4g", floatingVoltage);
+        Lcd_Write_String(floatingVoltage);
+        __delay_ms(200);
+    }
 }
