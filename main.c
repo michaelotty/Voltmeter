4b77561323360d161674b590d535402b714de9da
lk411 <lk411@EEPC-269.campus.bath.ac.uk>
2016-04-19 16:50:23 +0100

    added some button functionality

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 #pragma config  PWRTE = ON
 #pragma config  CP = OFF
 
+//#include <stdio.h>
 #include <xc.h>
 #include "adc.h"
 #include "lcd.h"
 
 #define _XTAL_FREQ  4000000
+#define BUTTON PORTAbits.RA4
 
 void main()
 {
-	TRISA = 0b00000100;
+	TRISA = 0b00010100;
 	TRISB = 0x00;
 
 	CS = 1;
@@ -20,23 +22,62 @@ void main()
 	Lcd_Init();
 	Lcd_Clear();
 
-	unsigned int Voltage = 5;
+	unsigned int voltage = 5;
 	while (1)
 	{
-		Lcd_Clear();
-
+	//	Lcd_Clear();                                     
+        
 		Lcd_Set_Cursor(1, 1);
 		char *string = "Voltage:";
-
+                   
 		Lcd_Write_String(string);
 		Lcd_Set_Cursor(2, 1);
 
-		Voltage = (readADC() * 4.5) / 1020;
-		Lcd_Write_Int(Voltage / 10000); //not sure what maximum number of chars is
-		/*Do we need to maybe set the cursor?*/ Lcd_Set_Cursor(2, 2);
-		Lcd_Write_Char(".");
-		Lcd_Set_Cursor(2, 3);
-		Lcd_Write_Int(Voltage % 10000); 
-		__delay_ms(200);
+		voltage = (readADC() * 5) / 1020;
+
+        //Lcd_Write_Int(voltage);
+                                                                                                                                                                                                                                                                                                                                                               
+        int decimal;
+        decimal = ((readADC() * 5) % 1020);
+        //ones = (voltage / 10000);
+        Lcd_Write_Int(voltage);
+        Lcd_Write_Char('.');
+        if (decimal < 10)
+        {
+            Lcd_Write_Int(0);
+            Lcd_Write_Int(0);
+            Lcd_Write_Int(decimal);
+        }
+        else if (decimal < 100)
+        {
+            Lcd_Write_Int(0);
+            Lcd_Write_Int(decimal);
+        }
+        else
+        {
+            Lcd_Write_Int(decimal);
+        }
+        
+        if(BUTTON)
+        {
+            //int run = 1;               
+                while (BUTTON) 
+                {
+                    __delay_ms(50);
+                }
+                while (BUTTON == 0) 
+                {
+                       __delay_ms(50);
+                }
+                while (BUTTON) 
+                {
+                       __delay_ms(50);
+                }
+                while(BUTTON == 0)
+                {
+                       __delay_ms(50);
+                }
+                
+        }
 	}
 }
