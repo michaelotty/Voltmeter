972c6c361c17fdd0b515fba38ba7f6b8e594590b
lk411 <lk411@EEPC-248.campus.bath.ac.uk>
2016-04-21 11:09:36 +0100

    added more button functionality

--- main.c
+++ main.c
@@ -9,75 +9,87 @@
 #include "lcd.h"
 
 #define _XTAL_FREQ  4000000
-#define BUTTON PORTAbits.RA4
+#define BUTTON RB0
+
+void WriteVoltage(unsigned int vTemp)
+{
+    	unsigned int voltage = 5;
+        voltage = (vTemp * 5) / 1020;
+        unsigned int decimal = ((vTemp * 5) % 1020);
+        Lcd_Write_Int(voltage);
+        Lcd_Write_Char('.');
+        if (decimal < 10)
+        {
+            Lcd_Write_Char('0');
+            Lcd_Write_Char('0');
+            Lcd_Write_Int(decimal);
+        }
+        else if (decimal < 100)
+        {
+            Lcd_Write_Char('0');
+            Lcd_Write_Int(decimal);
+        }
+        else
+        {
+           Lcd_Write_Int(decimal);
+        }
+}
+
+//void interrupt isr()
+//{
+//    INTCONbits.INTF = 0;
+//    __delay_ms(200);
+//    int run = 1;
+//    while(run)
+//    {
+//       run = !BUTTON;
+//    }
+//}
 
 void main()
 {
-	TRISA = 0b00010100;
-	TRISB = 0x00;
+	TRISA = 0b00000100;
+	TRISB = 0b00000001;
+    
+
 
 	CS = 1;
 	CLK = 0;
 	RW = 0;
 	Lcd_Init();
 	Lcd_Clear();
-
-	unsigned int voltage = 5;
+    
+   Lcd_Write_String("Welcome! The range is 0V to 5V");
+   int i =0;
+   while(i < 30)
+   {
+       __delay_ms(250);
+       Lcd_Shift_Left();
+       i++;
+   }
 	while (1)
-	{
-	//	Lcd_Clear();                                     
-        
-		Lcd_Set_Cursor(1, 1);
-		char *string = "Voltage:";
-                   
-		Lcd_Write_String(string);
-		Lcd_Set_Cursor(2, 1);
+	{                
+        //__delay_ms(1000);
+        Lcd_Clear();
+		Lcd_Set_Cursor(1,1);
+        Lcd_Write_Char('V');
+        Lcd_Write_Char('1');
+        Lcd_Write_Char(':');
 
-		voltage = (readADC() * 5) / 1020;
-
-        //Lcd_Write_Int(voltage);
-                                                                                                                                                                                                                                                                                                                                                               
-        int decimal;
-        decimal = ((readADC() * 5) % 1020);
-        //ones = (voltage / 10000);
-        Lcd_Write_Int(voltage);
-        Lcd_Write_Char('.');
-        if (decimal < 10)
+        WriteVoltage(readADC());
+        __delay_ms(20);
+        if(BUTTON == 1)
         {
-            Lcd_Write_Int(0);
-            Lcd_Write_Int(0);
-            Lcd_Write_Int(decimal);
-        }
-        else if (decimal < 100)
-        {
-            Lcd_Write_Int(0);
-            Lcd_Write_Int(decimal);
-        }
-        else
-        {
-            Lcd_Write_Int(decimal);
+            while(1)
+            {
+                __delay_ms(100);
+                if(BUTTON == 1)
+                {
+                    __delay_ms(100);
+                    break;
+                }
+            }
         }
         
-        if(BUTTON)
-        {
-            //int run = 1;               
-                while (BUTTON) 
-                {
-                    __delay_ms(50);
-                }
-                while (BUTTON == 0) 
-                {
-                       __delay_ms(50);
-                }
-                while (BUTTON) 
-                {
-                       __delay_ms(50);
-                }
-                while(BUTTON == 0)
-                {
-                       __delay_ms(50);
-                }
-                
-        }
 	}
 }
