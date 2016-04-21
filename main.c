a4edda962f200b6b787bf74320fef19d788c6858
LabKay <laurabellek@yahoo.com>
2016-04-21 16:39:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #pragma config  PWRTE = ON
 #pragma config  CP = OFF
 
+//#include <stdio.h>
 #include <xc.h>
 #include "adc.h"
 #include "lcd.h"
@@ -10,79 +11,105 @@
 #define _XTAL_FREQ  4000000
 #define BUTTON RA4
 
-void WriteVoltage(unsigned int vTemp) {
-    unsigned int voltage = 5;
+void WriteVoltage(unsigned int vTemp)
+{
+    unsigned int voltage;
     voltage = (vTemp * 5) / 1020;
     unsigned int decimal = (vTemp * 5) % 1020;
     Lcd_Write_Int(voltage);
     Lcd_Write_Char('.');
-    if (decimal < 10) {
+    if (decimal < 10)
+    {
         Lcd_Write_Char('0');
         Lcd_Write_Char('0');
         Lcd_Write_Int(decimal);
-    } else if (decimal < 100) {
+    }
+    else if (decimal < 100)
+    {
         Lcd_Write_Char('0');
         Lcd_Write_Int(decimal);
-    } else {
-        Lcd_Write_Int(decimal);
+    }
+    else
+    {
+       Lcd_Write_Int(decimal);
     }
 }
 
-void debounce() {
-    while (BUTTON) {
-        __delay_us(10);
-    }
+void debounce()
+{
+    while(BUTTON)
+            {
+                __delay_us(10);
+            }          
 }
 
-void ScrollText() {
-    short int i = 0;
-    while (i < 30) {
-        __delay_ms(250);
-        Lcd_Shift_Left();
-        i++;
-    }
-}
-
-void init() {
+void init()
+{
     Lcd_Init();
-    Lcd_Clear();
-
-    Lcd_Write_String("Welcome! The range is 0 to 5V");
-    Lcd_Set_Cursor(2, 1);
-    Lcd_Write_String(" The range is 0 to 5V");
+	Lcd_Clear();
+    
+   Lcd_Write_String("Welcome! The range is 0 to 5V");
+   Lcd_Set_Cursor(2,1);
+   Lcd_Write_String(" The range is 0 to 5V");
 }
 
-void main() {
-    TRISA = 0b00010100;
-    TRISB = 0b00001100;
+void ScrollText()
+{
+     short int i =0;
+     while(i < 30)
+   {
+       __delay_ms(250);
+       Lcd_Shift_Left();
+       i++;
+   }
+}
 
 
 
-    CS = 1;
-    CLK = 0;
+void main()
+{
+	TRISA = 0b00010100;
+	TRISB = 0b00001100;
+    
 
-    init();
-    ScrollText();
 
-    while (1) {
+	CS = 1;
+	CLK = 0;
+
+   init();
+   ScrollText();
+  
+	while (1)
+	{      
+        //__delay_ms(1000);
+#define DIN RA2
         Lcd_Clear();
-        Lcd_Set_Cursor(1, 1);
+		Lcd_Set_Cursor(1,1);
         Lcd_Write_Char('V');
-        Lcd_Write_Char('1');
         Lcd_Write_Char(':');
-
+       // DIN = PORTAbits.RA2;
         WriteVoltage(readADC());
-
-        __delay_ms(20);
-        if (BUTTON) {
+#undef DIN
+        Lcd_Set_Cursor(2,1);
+        Lcd_Write_Char('V');
+        Lcd_Write_Char(':');
+        //DIN = PORTBbits.RB3;
+#define DIN RB3
+        WriteVoltage(readADC());
+#undef DIN
+        __delay_ms(50);
+        if(BUTTON)
+        {
             debounce();
-            while (1) {
-                if (BUTTON) {
+            while(1)
+            {
+                if(BUTTON)
+                {
                     debounce();
                     break;
                 }
             }
         }
-
-    }
+        
+	}
 }
