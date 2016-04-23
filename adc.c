548c35c0269818ed1894cc2c38973279276ff25c
LabKay <laurabellek@yahoo.com>
2016-04-23 22:14:55 +0100

    No More Switch Case

--- adc.c
+++ adc.c
@@ -1,7 +1,6 @@
 #include "adc.h"
 #include <xc.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 // Read a single sample from the ADC
 unsigned int readADC(int selectedADC)
@@ -9,9 +8,6 @@ unsigned int readADC(int selectedADC)
   unsigned char bits;
   unsigned char rxData = 0;
   unsigned int volt = 0;
-  
-  // Pull CS low
-  CS = 0;
 
   // Three clock toggles while the ADC is converting (see data sheet)
   for (bits = 0; bits < 3; bits++)
@@ -23,8 +19,6 @@ unsigned int readADC(int selectedADC)
   }
 
   // Read in the 8 MSBs, ignore the two lower bits
-    switch (selectedADC) {
-        case 0:
             for (bits = 0; bits < 8; bits++) {
                 // Delay before we raise the clock
                 __delay_us(100);
@@ -34,37 +28,14 @@ unsigned int readADC(int selectedADC)
                 rxData = rxData << 1;
 
                 // Set or clear based on pin value
-                if (DIN0 == 1)
+                if (DIN == 1)
                     rxData = rxData | 0x01;
                 else
                     rxData = rxData & 0xfe;
 
                 __delay_us(100);
                 CLK = 0;
-            }
-            break;
-        case 1:
-            for (bits = 0; bits < 8; bits++) {
-                // Delay before we raise the clock
-                __delay_us(100);
-                CLK = 1;
 
-                // Read in the next bit, shift up by one bit
-                rxData = rxData << 1;
-
-                // Set or clear based on pin value
-                if (DIN1 == 1)
-                    rxData = rxData | 0x01;
-                else
-                    rxData = rxData & 0xfe;
-
-                __delay_us(100);
-                CLK = 0;
-            }
-            break;
-    }
-  // Clear the CS bit
-  CS = 1;
   
   // Return an integer, shift up by two because we only read 8-bits not 10.
   volt = (rxData << 2);
